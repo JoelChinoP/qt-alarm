fb7b99a9b6012647669732000747bf9f942b5fd7
Joel <jchinop@unsa.edu.pe>
2024-10-08 17:01:07 -0500

    rm build

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_alarms_FILE C:/Qt/Examples/Qt-6.8.0/tutorials/alarms/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/alarms.exe)
-set(__QT_DEPLOY_TARGET_alarms_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_alarms_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
